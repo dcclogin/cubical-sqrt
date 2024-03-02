4d17b270b50f14e67c91c15075a14a6b8a5aef28
sanad <sdkadu@iu.edu>
2024-03-02 11:53:51 -0500

    Denman–Beavers iteration fro sqrt.

--- sqrt/Sqrt.hs
+++ sqrt/Sqrt.hs
@@ -7,6 +7,7 @@ module Sqrt where
 import Data.List
 import Numeric.AD
 import Numeric.LinearAlgebra
+import qualified GHC.TypeLits as iteration
 -- import qualified Numeric.AD.Mode.Reverse as R
 -- import qualified Numeric.AD.Mode.Reverse.Double as RD
 
@@ -20,3 +21,6 @@ carleman size = [[ aux j k | k <- [0..size-1]] | j <- [0..size-1]]
     aux j k = if k < length (diffs (\x -> f x ^ j) 0) 
               then (diffs (\x -> f x ^ j) 0 !! k) / product [1.. fromIntegral k]
               else 0
+
+-- To find square root we can use Denman–Beavers iteration.
+sqrtm (fromLists (carleman 3))
